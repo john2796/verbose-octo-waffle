5b9bfdaa10c924329833d5082435247441c12a92
john2796 <benedictmiranda27@yahoo.com>
2018-10-19 21:07:52 -0700

    added styled components

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,18 +1,52 @@
 import React from 'react';
 
-import classes from './Toolbar.module.css';
+import styled from 'styled-components';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
 
+const ToolbarStyle = styled.div`
+  .Toolbar {
+    height: 56px;
+    width: 100%;
+    position: fixed;
+    top: 0;
+    left: 0;
+    background: #703b09;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0 20px;
+    box-sizing: border-box;
+    z-index: 90;
+
+    nav {
+      height: 100%;
+      background: red;
+    }
+  }
+
+  .Logo {
+    width: 80%;
+  }
+  /*  */
+  @media (max-width: 499px) {
+    .DesktopOnly {
+      display: none;
+    }
+  }
+`;
+
 const toolbar = props => {
   return (
-    <header className={classes.Toolbar}>
-      <div>Menu</div>
-      <Logo height="80%" />
-      <nav className={classes.DesktopOnly}>
-        <NavigationItems />
-      </nav>
-    </header>
+    <ToolbarStyle>
+      <header className="Toolbar">
+        <div>Menu</div>
+        <Logo height="80%" className="Logo" />
+        <nav className="DesktopOnly">
+          <NavigationItems />
+        </nav>
+      </header>
+    </ToolbarStyle>
   );
 };
 
